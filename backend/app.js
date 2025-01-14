41f550f192a7b7b7f8f8564be86ab1e00e5ced68
rohitranjanab1718 <rohitranjanab1718@gmail.com>
2025-01-14 19:57:27 +0530

    backend fix

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+require("dotenv").config();
 app.use(express.json());
 
 const dishRouter = require('./routes/dishRouter');
 
 app.use("/api",dishRouter);
-const port = '3010'
-app.listen(port,()=>{
-    console.log('server is running on '+ port);
+// const port = '3010'
+app.listen(process.env.port,()=>{
+    console.log('server is running on '+ process.env.port);
 })
